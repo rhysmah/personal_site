6599c2e9378fc773614c8de85785cc679ed05460
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-08-19 14:47:20 -0700

    feat: parse and execute HTML data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hi, my name is Rhys"))
+
+	// Attempt to read template file into template set (ts)
+	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+	if err != nil {
+
+		// Log the error, then send error for display to the user
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return // return so no subsequent code is run
+	}
+
+	// Write template content as response body
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func resumeHandler(w http.ResponseWriter, r *http.Request) {

257078256429db52f74eb4eacafb9aa5cfa377a3
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-04-20 09:44:29 -0700

    feature: add simple local network port flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /about", about)
 
-	log.Print("starting server on :4000")
+	log.Printf("starting server on %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }

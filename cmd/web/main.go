5ffc28c35ab370aaaae7d834cd848ee555f6080b
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-04-23 11:14:43 -0700

    feature: update functions as struct methods

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,21 +2,42 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 
+	// CLI-based configuration
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /about", about)
+	// Structured Logger
+	// Setting minimum level to LevelDebug, which are the least severe
+	// and are, by default, silently discarded from logs
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true, // includes filename and log number
+	}))
 
-	log.Printf("starting server on %s", *addr)
+	// Instantiate the struct that'll use the logger
+	app := &application{
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /about", app.about)
+
+	// .Any() prevents !BADKEY errors
+	logger.Info("Starting server", slog.Any("addr", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error()) // Human-readable message
+	os.Exit(1)                // No equivalent to log.Fatal(), which calls this automatically
 }

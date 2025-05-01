0773f51db63953d5860d77a4baaf3e596c1db651
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-05-01 14:02:08 -0700

    feature: Add a FileServer for static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,6 +6,9 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /about", app.about)
 

894abefbbe6e2d3ded25d7d2124e9201e2c4ad9c
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-05-01 12:44:38 -0700

    feature: write server error methods

---
+++ cmd/web/helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+// serverError writes a log entry at Error level (including the request methods and URI as
+// attributes), then sends a generic 500 Internal Server Error response to the user.
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(
+		err.Error(),
+		slog.Any("method", method),
+		slog.Any("uri", uri))
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}

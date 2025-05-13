5fbee02f899d73252fbfaf7ef053e20e23e06148
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-05-12 19:05:02 -0700

    feature: add basic middleware, middleware chain

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 // routes returns a multiplexer
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -12,5 +12,10 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /about", app.about)
 
-	return mux
+	// middleware
+	handler := app.secureHeaders(mux)
+	handler = app.logRequest(handler)
+	handler = app.recoverPanic(handler)
+
+	return handler
 }
